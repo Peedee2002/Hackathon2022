50afca10da9aedc791f75deb96abeeb8201d010e
Srija Mukherjee <srijamukherjee@Srijas-MBP-2.modem>
2022-07-17 11:20:57 +1000

    added IBM watson stuff

--- try.js
+++ try.js
@@ -4,26 +4,34 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 // https://cloud.ibm.com/apidocs/natural-language-understanding?code=node#features-examples
 // this is from the official IBM tutorial
 
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-  version: '2022-04-07',
-  authenticator: new IamAuthenticator({
-    apikey: '_faoS8mAvH-l3VhkgBFElVvtxJRTbsuRUfrSbWGec549',
-  }),
-  serviceUrl: 'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/6f0caca6-4a6b-4260-8119-d2ea0633b9cb',
-});
-
-const analyzeParams = {
-    'text': 'Apples and Oranges I love apples! I dont like oranges.',
-    'features': {
-      'emotion': {
-      }
-    }
-  };
-
-naturalLanguageUnderstanding.analyze(analyzeParams)
-  .then(analysisResults => {
-    console.log(JSON.stringify(analysisResults, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
+function bigFunction (text) { 
+  const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2022-04-07',
+    authenticator: new IamAuthenticator({
+      apikey: '_faoS8mAvH-l3VhkgBFElVvtxJRTbsuRUfrSbWGec549',
+    }),
+    serviceUrl: 'https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/6f0caca6-4a6b-4260-8119-d2ea0633b9cb',
   });
+  
+  const analyzeParams = {
+      'text': text,
+      'features': {
+        'emotion': {
+        }
+      }
+    };
+  
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+    .then(analysisResults => {
+      console.log(JSON.stringify(analysisResults.result.emotion.document.emotion, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+}
+
+texts = ['Hey, you are disgusting', 'You are amazing!!']
+
+texts.forEach(text => {
+  bigFunction (text)
+});
